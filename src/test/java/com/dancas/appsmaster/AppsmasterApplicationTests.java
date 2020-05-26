a7cc3b818b209f38ab065123f6713285630e2390
Daniel Castilla <danielcastilla@gmail.com>
2020-05-26 13:59:15 +0200

    initial commit

---
+++ src/test/java/com/dancas/appsmaster/AppsmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dancas.appsmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
