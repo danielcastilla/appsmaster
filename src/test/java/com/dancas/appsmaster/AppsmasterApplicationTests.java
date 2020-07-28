f1fbe2a8f3eb067c6f51112fe69be3551b60c8b9
danielcastilla <danielcastilla@gmail.com>
2020-07-28 17:37:05 +0200

    Added first test

--- src/test/java/com/dancas/appsmaster/AppsmasterApplicationTests.java
+++ src/test/java/com/dancas/appsmaster/AppsmasterApplicationTests.java
@@ -1,13 +1,15 @@
 package com.dancas.appsmaster;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class AppsmasterApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AppsmasterApplication.class)
+public class AppsmasterApplicationTests {
 
-	//@Test
-	void contextLoads() {
+	@Test
+	public void whenSpringContextIsBootstrapped_thenNoExceptions() {
 	}
-
 }
