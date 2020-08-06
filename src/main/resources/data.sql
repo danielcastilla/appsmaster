e8019b936d859d04c6431371e58b9f5931c04df0
danielcastilla <danielcastilla@gmail.com>
2020-08-06 12:52:14 +0200

    Added sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,10 +5,10 @@ CREATE TABLE applications (
   name VARCHAR(50) NOT NULL,
   mail VARCHAR(50) NOT NULL,
   description VARCHAR(250) NOT NULL,
-  date DATE NOT NULL
+  initDate date NOT NULL
 );
 
-INSERT INTO applications (name, mail, description, date) VALUES
-  ('Application1', 'mail1@domain.com','1 Description of application number one', 1-8-2020),
-  ('Application2', 'mail2@domain.com','2 Description of application number two', 1-8-2020),
-  ('Application3', 'mail3@domain.com','3 Description of application other application', 1-8-2020);
+INSERT INTO applications (name, mail, description, initDate) VALUES
+  ('Application1', 'mail1@domain.com','1 Description of application number one', '2020-8-1'),
+  ('Application2', 'mail2@domain.com','2 Description of application number two', '2020-8-1'),
+  ('Application3', 'mail3@domain.com','3 Description of application other application', '2020-8-1');
